afd966b4cd49d68729006e72376b1a3d95f690d6
Steve Hartken <steve.hartken@gmail.com>
2019-05-28 15:58:42 -0400

    Received approval to switch from on page form to Stripe checkout dialog
    Styles and HTML
    
    Prettier

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,28 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeRoutingModule } from './home-routing.module';
-import { SharedModule } from '../shared/shared.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { OwlModule } from 'ngx-owl-carousel';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeRoutingModule } from "./home-routing.module";
+import { SharedModule } from "../shared/shared.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { OwlModule } from "ngx-owl-carousel";
 
-import { HomeComponent } from './home.component';
-import { IntroComponent } from './intro/intro.component';
-import { AboutComponent } from './about/about.component';
-import { FeatureComponent } from './feature/feature.component';
-import { TestimonialComponent } from './testimonial/testimonial.component';
-import { ScreenshotComponent } from './screenshot/screenshot.component';
-import { HowItWorkComponent } from './how-it-work/how-it-work.component';
-import { TeamComponent } from './team/team.component';
-import { PriceComponent } from './price/price.component';
-import { BlogComponent } from './blog/blog.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { FaqComponent } from './faq/faq.component';
-import { DownloadComponent } from './download/download.component';
-import { NewsLetterComponent } from './news-letter/news-letter.component';
-import { VideosComponent } from '../videos/videos.component';
-
-import { StripeDemoComponent } from '../stripe-demo/stripe-demo.component';
-
+import { HomeComponent } from "./home.component";
+import { IntroComponent } from "./intro/intro.component";
+import { AboutComponent } from "./about/about.component";
+import { FeatureComponent } from "./feature/feature.component";
+import { TestimonialComponent } from "./testimonial/testimonial.component";
+import { ScreenshotComponent } from "./screenshot/screenshot.component";
+import { HowItWorkComponent } from "./how-it-work/how-it-work.component";
+import { TeamComponent } from "./team/team.component";
+import { PriceComponent } from "./price/price.component";
+import { BlogComponent } from "./blog/blog.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
+import { FaqComponent } from "./faq/faq.component";
+import { DownloadComponent } from "./download/download.component";
+import { NewsLetterComponent } from "./news-letter/news-letter.component";
+import { VideosComponent } from "../videos/videos.component";
 
 @NgModule({
   imports: [
@@ -31,7 +28,7 @@ import { StripeDemoComponent } from '../stripe-demo/stripe-demo.component';
     SharedModule,
     ReactiveFormsModule,
     FormsModule,
-    OwlModule
+    OwlModule,
   ],
   declarations: [
     HomeComponent,
@@ -49,7 +46,6 @@ import { StripeDemoComponent } from '../stripe-demo/stripe-demo.component';
     DownloadComponent,
     NewsLetterComponent,
     VideosComponent,
-    StripeDemoComponent
-  ]
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
