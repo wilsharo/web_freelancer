f3ee1dbf3dbc6fad68f085da3de119792e239b59
Steve Hartken <steve.hartken@gmail.com>
2019-05-28 20:29:15 -0400

    #3 -- Create purchase subscription page
    Reference:
    https://angularfirebase.com/lessons/stripe-recurring-subscription-payments-with-angular-and-firebase-cloud-functions/#Triggering-the-Stripe-Checkout-Modal

--- src/app/home/intro/intro.component.ts
+++ src/app/home/intro/intro.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PaymentServiceService } from "../../payments/payment-service.service";
+import { environment } from "../../../environments/environment";
 
 @Component({
-  selector: 'app-intro',
-  templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  selector: "app-intro",
+  templateUrl: "./intro.component.html",
+  styleUrls: ["./intro.component.scss"],
 })
 export class IntroComponent implements OnInit {
+  handler: any;
 
-  constructor() { }
+  constructor(public pmt: PaymentServiceService) {}
 
   ngOnInit() {
+    this.configHandler();
   }
 
+  private configHandler() {
+    this.handler = StripeCheckout.configure({
+      key: environment.stripeKey,
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/urltv-mcbattlearena.appspot.com/o/urltv_web_images%2Furltv_orig_log_transparent.PNG?alt=media&token=9498df7f-569a-4753-9b26-98e7f0ee843d",
+      locale: "auto",
+      token: token => {
+        this.pmt.processPayment(token);
+      },
+    });
+  }
+
+  openHandler() {
+    this.handler.open({
+      name: "FireStarter",
+      excerpt: "PRO Subscription",
+      amount: 1500,
+    });
+  }
 }
